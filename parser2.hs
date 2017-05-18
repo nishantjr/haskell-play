744438e30e9398a9acf175e14467a27f4a1bc13a
Nishant Rodrigues <nishantjr@gmail.com>
2017-05-18 13:08:43 -0500

    parser2: expanderator: expands "compressed" strings
    e.g. "2.3-1^3-2." → "..---^--.."

--- parser2.hs
+++ parser2.hs
@@ -1,7 +1,16 @@
 {-# LANGUAGE RebindableSyntax #-}
 
-import Prelude (IO, String, Char, Maybe (Nothing, Just),
-    print, fromInteger, const, ($))
+import Prelude (
+    IO, String, Char, Maybe (Nothing, Just),
+    print, putStrLn, fromInteger, const, ($), (++), (==), Bool (True, False), Int, read,
+    foldr, repeat, take, fst)
+import Data.Char
+
+--- XXX
+ifThenElse :: Bool -> a -> a -> a
+ifThenElse True a _ = a
+ifThenElse False _ a = a
+
 
 type Parser a = String -> Maybe (a, String)
 
@@ -27,13 +36,51 @@ char :: Parser Char
 char = \s -> case s of x:xs -> Just (x, xs)
                        ""   -> Nothing
 
+sat :: Parser a -> (a -> Bool) -> Parser a
+sat p f = do x <- p
+             if f x then return x
+             else fail
+
+literal :: Char -> Parser Char
+literal c = sat char (== c)
+
+digit :: Parser Char
+digit = sat char isDigit
+
+letter :: Parser Char
+letter = sat char isLetter
+
+(+++) :: Parser a -> Parser a -> Parser a
+a +++ b = \s -> case parse a s of Nothing -> parse b s
+                                  x       -> x
+
+many1 :: Parser a -> Parser [a]
+many1 p = do x  <- p
+             xs <- many p
+             return (x:xs)
+
+many :: Parser a -> Parser [a]
+many p = many1 p +++ return []
+
+nat :: Parser Int
+nat = do ds <- many digit
+         return (read ds)
+
+expanderator :: Parser String
+expanderator = do segments <-  many termExpandor
+                  return (foldr (++) [] segments)
+    where termExpandor :: Parser String
+          termExpandor = do n <- nat
+                            c <- char
+                            return (take n (repeat c))
+
 secondChar :: Parser Char
 secondChar =
      do char
         char
 
 firstThird :: Parser (Char, Char)
-firstThird = do x <- char
+firstThird = do x <- digit +++ letter
                 char
                 y <- char
                 return (x, y)
@@ -43,4 +90,8 @@ main =
 -- print $ (return 3) "hello"
 -- print $ char "hello"
 -- print $ secondChar "hello"
-   print $ firstThird "hello"
+-- print $ (many (digit +++ letter)) "2sdd"
+   putStrLn $ case expanderator expr of
+                    Just (x, s) -> x
+                    Nothing     -> "Oops."
+              where expr = "2 1\\1\n3 1\\1\n2 1/1 1\\1\n1 1/3 1\\1\n1/5 1\\1\n" 
