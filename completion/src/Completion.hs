6efa27718006c096cdca82f593eb3b9f64cc3e85
Nishant Rodrigues <nishantjr@gmail.com>
2017-05-27 13:09:29 +0900

    completion/src/Completion.hs

---
+++ completion/src/Completion.hs
@@ -0,0 +1,7 @@
+module Completion where
+
+// XXX we leave out the database of sentence priorities (which ones are
+//     more commonly chosen, etc.)
+
+complete :: Sentences -> Words -> Sentences
+complete ss ws = undefined
